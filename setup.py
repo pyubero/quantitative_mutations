43cacfe96aa496bd3a6646a1a8dad5be09051020
Pablo Yubero <pabloyub@hotmail.com>
2022-06-17 10:54:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
 	name = 'qmut', 
 	version = "0.0.1", 
 	author = "Pablo Yubero",  
-	description = "This package contains a class that can handle the quantitative mutations in cobra models.",
+	description = "This package contains a class that can handle quantitative mutations in cobra models. More info available at 'Pablo Yubero, Alvar A. Lavin, Juan F. Poyatos bioRxiv 2022.05.19.492732' ",
 	packages = ["cobrapy_qmut"],
 	install_requires=['numpy','cobra', 'tqdm', 'datetime', 'matplotlib']  ,
 	reference = "Pablo Yubero, Alvar A. Lavin, Juan F. Poyatos bioRxiv 2022.05.19.492732",
